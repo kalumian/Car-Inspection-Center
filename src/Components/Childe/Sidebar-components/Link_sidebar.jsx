b2913a4b643f0bc6860e0e4f0e00c2e3874412a6
kalumian <tradation10@gmail.com>
2021-08-22 22:52:38 +0300

    Sidebar Finish

--- src/Components/Childe/Sidebar-components/Link_sidebar.jsx
+++ src/Components/Childe/Sidebar-components/Link_sidebar.jsx
@@ -1,13 +1,11 @@
 import { Link, useLocation } from "react-router-dom";
-
 function Link_sidebar({ title, icon }) {
   const location = useLocation();
-
   return (
     <Link
-      href={title}
+      to={title.split(" ").join("-")}
       className={`list-group-item list-group-item-action bg-transparent second-text fw-bold ${
-        location.pathname === "title" ? "active" : ""
+        location.pathname === "/" + title.split(" ").join("-") ? "active" : ""
       }`}
     >
       <i className={icon + " me-2"}></i>
--- src/Components/Pages/Sidebar.jsx
+++ src/Components/Pages/Sidebar.jsx
@@ -15,61 +15,10 @@ function Sidebar() {
       </div>
       {/* ----------- Links -----------*/}
       <div className="list-group list-group-flush my-3">
-        <a
-          href="#"
-          className="list-group-item list-group-item-action bg-transparent second-text fw-bold"
-        >
-          <i className="fas fa-tachometer-alt me-2"></i>Dashboard
-        </a>
-        <Link_sidebar />
-        <a
-          href="#"
-          className="list-group-item list-group-item-action bg-transparent second-text fw-bold"
-        >
-          <i className="fas fa-project-diagram me-2"></i>Projects
-        </a>
-        <a
-          href="#"
-          className="list-group-item list-group-item-action bg-transparent second-text fw-bold"
-        >
-          <i className="fas fa-chart-line me-2"></i>Analytics
-        </a>
-        <a
-          href="#"
-          className="list-group-item list-group-item-action bg-transparent second-text fw-bold"
-        >
-          <i className="fas fa-paperclip me-2"></i>Reports
-        </a>
-        <a
-          href="#"
-          className="list-group-item list-group-item-action bg-transparent second-text fw-bold"
-        >
-          <i className="fas fa-shopping-cart me-2"></i>Store Mng
-        </a>
-        <a
-          href="#"
-          className="list-group-item list-group-item-action bg-transparent second-text fw-bold"
-        >
-          <i className="fas fa-gift me-2"></i>Products
-        </a>
-        <a
-          href="#"
-          className="list-group-item list-group-item-action bg-transparent second-text fw-bold"
-        >
-          <i className="fas fa-comment-dots me-2"></i>Chat
-        </a>
-        <a
-          href="#"
-          className="list-group-item list-group-item-action bg-transparent second-text fw-bold"
-        >
-          <i className="fas fa-map-marker-alt me-2"></i>Outlet
-        </a>
-        <a
-          href="#"
-          className="list-group-item list-group-item-action bg-transparent text-danger fw-bold"
-        >
-          <i className="fas fa-power-off me-2"></i>Logout
-        </a>
+        <Link_sidebar title="الفواتير" icon="fas fa-file-invoice" />
+        <Link_sidebar title="استعلام" icon="fas fa-search" />
+        <Link_sidebar title="قائمة الكروت" icon="far fa-list-alt" />
+        <Link_sidebar title="الإعدادات" icon="fas fa-cogs" />
       </div>
     </div>
   );
