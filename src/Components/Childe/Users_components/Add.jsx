c8c5dff92fd8e450fa794247027db04119b57f62
kalumian <tradation10@gmail.com>
2022-02-02 08:41:22 +0300

    -

--- src/Components/Childe/Users_components/Add.jsx
+++ src/Components/Childe/Users_components/Add.jsx
@@ -7,49 +7,59 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Add() {
-  // States for registration
+  // States & Fucnctions
   const [username, setUsername] = useState("");
-  const [iDNumber, setIdNumber] = useState("");
+  const [identity, setIdentity] = useState("");
   const [password, setPassword] = useState("");
   const [account, setAccount] = useState({});
   const [message, setMessage] = useState("");
   const [type, setType] = useState("");
   const [styleButton, setStyleButton] = useState(false);
-
+  const deletContent = () => {
+    setUsername("");
+    setPassword("");
+    setIdentity("");
+    setType("");
+  };
   const handleSubmit = (e) => {
     setStyleButton(true);
     e.preventDefault();
-    setAccount({ password, username, type });
+    setAccount({
+      password:"12345",
+      name: "11223344",
+      type:"Reception",
+      identity:"Alwalead",
+    });
     setTimeout(() => {
       setStyleButton(false);
     }, 4000);
   };
 
   useEffect(async () => {
-    try {
-      let res = await fetch(
-        "https://peaceful-depths-13311.herokuapp.com/add-user",
-        {
-          method: "POST",
-          body: JSON.stringify(account),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      let resJson = await res.json();
-      console.log(res.status);
-      setUsername("");
-      setPassword("");
-      setMessage(res.status === 200 ? "تم اضافة الحساب بنجاح." : "");
-      setTimeout(() => {
-        setMessage("");
-      }, 3000);
-    } catch (err) {
-      setMessage(String(err));
-      setTimeout(() => {
-        setMessage("");
-      }, 3000);
+    if (Boolean(account.name) === true) {
+      try {
+        let res = await fetch(
+          "https://peaceful-depths-13311.herokuapp.com/register",
+          {
+            method: "POST",
+            body: JSON.stringify(account),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        let resJson = await res.json();
+        deletContent();
+        setMessage(res.status === 200 ? "تم اضافة الحساب بنجاح." : "");
+        setTimeout(() => {
+          setMessage("");
+        }, 3000);
+      } catch (err) {
+        setMessage(String(err));
+        setTimeout(() => {
+          setMessage("");
+        }, 3000);
+      }
     }
   }, [account]);
 
@@ -61,15 +71,15 @@ function Add() {
             <Lable
               active={false}
               For="invoices_customer_name"
-              title="اسم المستخدم "
+              title="اسم الموظف "
             />
             <input
               name="name_customer"
               id="invoices_customer_name"
               type="text"
               className="form-control"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              value={identity}
+              onChange={(e) => setIdentity(e.target.value)}
             />
           </div>
           <div className="col-4">
@@ -98,8 +108,8 @@ function Add() {
               id="invoices_customer_password"
               type="password"
               className="form-control"
-              value={iDNumber}
-              onChange={(e) => setIdNumber(e.target.value)}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
 
@@ -107,15 +117,15 @@ function Add() {
             <div className="col-4">
               <select
                 className="form-select"
-                id=""
                 onChange={(e) => setType(e.target.value)}
+                value={type}
               >
-                <option selected value={undefined}>
+                <option selected value={"undefined"}>
                   الوظيفة
                 </option>
-                <option id="SuperVisor">مشرف </option>
-                <option id="Reception">استقبال</option>
-                <option id="fitter">فني </option>
+                <option value={"SuperVisor"}>مشرف </option>
+                <option value={"Reception"}>استقبال</option>
+                <option value={"fitter"}>فني </option>
               </select>
             </div>
             <div className="col-4">
--- src/Components/Parents/Login/Login.jsx
+++ src/Components/Parents/Login/Login.jsx
@@ -1,6 +1,7 @@
 // Import from Lib
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useState, useEffect } from "react";
+import base64 from "base-64";
 
 // Import Images
 import logo from "../../../Assets/logo-no-bg.png";
@@ -9,22 +10,65 @@ import logo from "../../../Assets/logo-no-bg.png";
 import { DataContext } from "../../../DataContext";
 
 function Login() {
-  const { setUser } = useContext(DataContext);
   const year = new Date().getFullYear();
+
+  // Use State
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [message, setMessage] = useState();
+  const [account, setAccount] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setAccount({ password, username });
+  };
+
+  useEffect(async () => {
+    if (Boolean(account) === true) {
+      const headers = new Headers();
+      headers.set(
+        "Authorization",
+        "Basic " + base64.encode("Alwalead" + ":" + "12345")
+      );
+      try {
+        let res = await fetch(
+          "https://peaceful-depths-13311.herokuapp.com/login",
+          {
+            method: "POST",
+            headers,
+          }
+        );
+        let resJson = await res.json();
+        if (res.status === 200) {
+          console.log(resJson);
+          setMessage("work");
+        }
+      } catch (err) {
+        setMessage(" حدث خطأ فضلاً تأكد من المدخلات او شبكة الانترنت");
+        setTimeout(() => {
+          setMessage("");
+        }, 3000);
+      }
+    }
+  }, [account]);
   return (
     <section className="login d-flex justify-content-center align-items-center vh-100">
-      <form style={{ width: "19rem" }} className="text-center">
+      <form
+        style={{ width: "19rem" }}
+        className="text-center"
+        onSubmit={handleSubmit}
+      >
         <img class="mb-3" src={logo} alt="" width="200" />
         <div class="form-floating mb-3 text-right">
           <input
-            type="email"
+            type="text"
             class="form-control"
             id="floatingInput"
             autoComplete="off"
             placeholder="name@example.com"
-            // ---- Set User ---
+            value={username}
             onChange={({ target }) => {
-              setUser(target.value);
+              setUsername(target.value);
             }}
           />
           <label for="floatingInput">رقم الهوية</label>
@@ -36,15 +80,18 @@ function Login() {
             id="floatingPassword"
             placeholder="Password"
             autoComplete="off"
+            value={password}
+            onChange={({ target }) => {
+              setPassword(target.value);
+            }}
           />
           <label for="floatingPassword">كلمة المرور</label>
         </div>
         {/* ------------ Link ---------------- */}
-        <Link to="/">
-          <button className="w-100 btn btn-lg primary-bg" type="submit">
-            تسجيل الدخول
-          </button>
-        </Link>
+        <button className="w-100 btn btn-lg primary-bg" type="submit">
+          تسجيل الدخول
+        </button>
+        <div className="test-danger">{message}</div>
         <p class="mt-4 mb-3 text-muted">&copy; 2020 - {year}</p>
       </form>
     </section>
